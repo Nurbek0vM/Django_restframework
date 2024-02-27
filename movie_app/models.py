199debe388f2d04bfed9a708a3093cfa3d2ba824
Nurbek0vM <Nurbekov.mairambek5@mail.ru>
2024-02-28 01:51:26 +0600

    first commit

---
+++ movie_app/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Director(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField(blank=True, null=True)
+    duration = models.DurationField(blank=True, null=True)
+    director = models.ForeignKey(Director, on_delete=models.CASCADE, related_name="movies")
+
+    def __str__(self):
+        return self.title
+
+class Review(models.Model):
+    text = models.TextField()
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="reviews")
+
+    def __str__(self):
+        return self.text
