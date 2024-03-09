59012ff2142d4c7afece5799768115bcb8e5984b
Nurbek0vM <Nurbekov.mairambek5@mail.ru>
2024-03-10 03:37:24 +0600

    fourth commit2

--- movie_app/serializers.py
+++ movie_app/serializers.py
@@ -1,11 +1,10 @@
 from rest_framework import serializers
-
-from movie_app.models import Director, Movie, Review
+from .models import Director, Movie, Review
 
 
 def validate_name_min_length(value, min_length):
     if len(value) < min_length:
-        raise serializers.ValidationError(f'Минимальная длина для поля равна {min_length}')
+        raise serializers.ValidationError(f'Имя должно содержать не менее {min_length} символов')
     return value
 
 
@@ -15,12 +14,12 @@ class DirectorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Director
         fields = '__all__'
-
     def get_movies_count(self, obj):
         return obj.movies.count()
 
-    def validate_name(self, value):
-        validate_name_min_length(value, 4)
+    def validate_text(self, value):
+        validate_name_min_length(value, 5)
+
 
 
 class ReviewSerializer(serializers.ModelSerializer):
@@ -29,33 +28,18 @@ class ReviewSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def validate_text(self, value):
-        validate_name_min_length(value, 4)
+        validate_name_min_length(value, 5)
 
 
 class MovieSerializer(serializers.ModelSerializer):
     reviews = ReviewSerializer(many=True, read_only=True)
     average_rating = serializers.SerializerMethodField()
-
     class Meta:
         model = Movie
         fields = '__all__'
-    def get_movies_count(self, obj):
-        return obj.movies.count()
-
-
-
-class MovieSerializer(serializers.ModelSerializer):
-    reviews = ReviewSerializer(many=True, read_only=True)
-    average_rating = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Movie
-        fields = '__all__'
-
     def get_average_rating(self, obj):
         total_stars = sum(review.stars for review in obj.reviews.all())
         num_reviews = obj.reviews.count()
-
         if total_stars > 0:
             return total_stars / num_reviews
         else:
